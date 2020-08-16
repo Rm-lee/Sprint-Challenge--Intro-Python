d426f3b3dac1412e5d549aafd7e7399ff6423095
roger-lee <rmlee0003@gmail.com>
2020-08-16 18:12:59 -0500

    created classes for oop1 file

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,27 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+class Vehicle:
+    pass
+
+class FlightVehicle(Vehicle):
+    #vehicle base class
+    pass
+class Starship(FlightVehicle):
+    #FlightVehicle base class
+    pass
+class Airplane(FlightVehicle):
+    #flightvehicle base class
+    pass
+
+class GroundVehicle(Vehicle):
+    #vehicle base class
+    pass
+class Car(GroundVehicle):
+    #GroundVehicle base class
+    pass
+
+class Motorcycle(GroundVehicle):
+    #GroundVehicle base class
+    pass
